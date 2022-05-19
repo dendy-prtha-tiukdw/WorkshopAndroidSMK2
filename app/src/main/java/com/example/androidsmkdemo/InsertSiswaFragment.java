2adc9387324a31ddac12e0ed76dd51ada9dd77c9
dendy prtha <prtha.dendy@gmail.com>
2022-05-19 16:21:25 +0700

    add firebase firestore example

--- app/src/main/java/com/example/androidsmkdemo/InsertSiswaFragment.java
+++ app/src/main/java/com/example/androidsmkdemo/InsertSiswaFragment.java
@@ -1,16 +1,26 @@
 package com.example.androidsmkdemo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class InsertSiswaFragment extends Fragment {
 
@@ -29,6 +39,7 @@ public class InsertSiswaFragment extends Fragment {
     private DBHelper mydb;
     Button btnSave;
     EditText editTextNama;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         //inisialisasi obyek DBHelper, tombol simpan,
@@ -41,8 +52,29 @@ public class InsertSiswaFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 //menginputkan data siswa dengan nama yang diinputkan pada database
-                mydb.insertSiswa(editTextNama.getText().toString());
+//                mydb.insertSiswa(editTextNama.getText().toString());
                 //karena terdapat data baru pada database, maka
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                String id = db.collection("siswa").document().getId();
+                Map<String, Object> siswa = new HashMap<>();
+                siswa.put("id", id);
+                siswa.put("nama", editTextNama.getText().toString());
+
+                db.collection("siswa")
+                        .add(siswa)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("TAG", "onSuccess input data siswa: " + id);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("TAG", "onFailure input data siswa " + e.getMessage());
+                            }
+                        });
+
                 //melakukan refresh daftar siswa pada fragment ListSiswaFragment
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 ListSiswaFragment fragm = (ListSiswaFragment) fm.findFragmentById(R.id.fragment_list_siswa);
--- app/src/main/java/com/example/androidsmkdemo/ListSiswaFragment.java
+++ app/src/main/java/com/example/androidsmkdemo/ListSiswaFragment.java
@@ -7,9 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -18,6 +25,7 @@ public class ListSiswaFragment extends Fragment {
     //inisialisasi variable SiswaAdapater dan lisSiswa
     private SiswaAdapter siswaAdapter;
     private ListView listSiswa;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -32,23 +40,58 @@ public class ListSiswaFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         //inisialisasi obyek DBHelper
         mydb = new DBHelper(getActivity());
-        //mendapatkan semua data siswa dari database
-        ArrayList arrayList = mydb.getAllSiswa();
+//        //mendapatkan semua data siswa dari database
+//        ArrayList arrayList = mydb.getAllSiswa();
         listSiswa = view.findViewById(R.id.list_siswa);
-
-        //memberikan data-data siswa ke
-        // adapter ListView siswa untuk dapat ditampilkan
-        siswaAdapter = new SiswaAdapter(getActivity(),0, arrayList);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        ArrayList<Siswa> array_list = new ArrayList<>();
+        db.collection("siswa")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            //looping dengan foreach
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Siswa siswa = new Siswa(document.getString("id")
+                                        , document.getString("nama"));
+                                array_list.add(siswa);
+                            }
+                        }
+                    }
+                });
+//        //memberikan data-data siswa ke
+//        // adapter ListView siswa untuk dapat ditampilkan
+        siswaAdapter = new SiswaAdapter(getActivity(), 0, array_list);
         listSiswa.setAdapter(siswaAdapter);
+
     }
 
     //method untuk meng-refresh ArrayList
     // bila ada perubahan data pada database
     public void refreshList() {
         Log.d(ListSiswaFragment.class.getName(), "refreshList: clicked");
-        ArrayList array_list = mydb.getAllSiswa();
-        siswaAdapter.clear();
-        siswaAdapter.addAll(array_list);
-        siswaAdapter.notifyDataSetChanged();
+//        ArrayList array_list = mydb.getAllSiswa();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        ArrayList<Siswa> array_list = new ArrayList<>();
+        db.collection("siswa")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            //looping dengan foreach
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Siswa siswa = new Siswa(document.getString("id")
+                                        , document.getString("nama"));
+                                array_list.add(siswa);
+                            }
+                        }
+                        siswaAdapter.clear();
+                        siswaAdapter.addAll(array_list);
+                        siswaAdapter.notifyDataSetChanged();
+                    }
+                });
+
     }
 }
